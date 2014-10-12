208e994b9098bbd2559553646b3f60eb8279841a
hurgleburgler <bubasti@gmail.com>
2014-10-11 21:01:09 -0700

    A good beginning is only half the task.

---
+++ marvelize/api.py
@@ -0,0 +1,10 @@
+from tastypie.resources import ModelResource
+from tastypie import fields
+#from marvelize.models import Foo 
+
+#class Resource(ModelResource):
+#    class Meta:
+#        queryset = Foo.objects.all()
+#        resource_name = 'resource'
+
+
